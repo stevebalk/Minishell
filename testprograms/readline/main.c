9b71a63a7e76ac7fdc9a8a33750c50535e689ef3
Jonas Peters <jopeters@c2s16d98.42wolfsburg.de>
2023-11-09 11:54:07 +0100

    testprograms/readline

---
+++ testprograms/readline/main.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jopeters <jopeters@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 11:48:54 by jopeters          #+#    #+#             */
+/*   Updated: 2023/11/09 11:53:15 by jopeters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+
+int main(void)
+{
+    char *input;
+
+    input = readline("Enter your name: ");
+
+    printf("Hello, %s!\n", input);
+
+    free(input);
+
+    return 0;
+}
+
+
+/*
+
+gcc -o main.c -lreadline
+
+*/
