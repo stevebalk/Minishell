849a0327e71947db2f36f2937727b12c94db918f
Steve Balk <steve.balk@web.de>
2023-11-14 16:05:13 +0100

    Splitted everything to it's own functions and made it norminette comform

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,11 +6,47 @@
 /*   By: sbalk <sbalk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 15:13:27 by sbalk             #+#    #+#             */
-/*   Updated: 2023/11/14 14:24:00 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/11/14 16:03:46 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/lexer.h"
+
+/* Creates a new empty token and appends it to
+the linked list. If there is no head token it
+will assignes the token as head */
+t_token	*token_list_append(t_token **head)
+{
+	t_token	*cur;
+
+	if (*head == NULL)
+	{
+		*head = create_token(*head);
+		return (*head);
+	}
+	else
+	{
+		cur = *head;
+		while (cur->next != NULL)
+			cur = cur->next;
+		cur->next = create_token(*head);
+	}
+	return (cur->next);
+}
+
+/* Checks string and sets the token type accordingly.
+Returns length of the token string length */
+int	set_token(t_token *token, char *str, t_token *lx)
+{
+	int	ret;
+
+	ret = 0;
+	if (ft_strchr(TOKEN_TYPES, *str))
+		ret += set_special_token(str, token, lx);
+	else
+		ret += set_word_token(str, token, lx);
+	return (ret);
+}
 
 /* Takes an input string and breaks it down into tokens.
 It creates and sets tokenparamters based on the given rules*/
@@ -32,20 +68,8 @@ t_token	*lexer(char *str)
 		}
 		if (*str == '\0')
 			return (head);
-		if (head != NULL)
-		{
-			cur_token->next = create_token(head);
-			cur_token = cur_token->next;
-		}
-		else
-		{
-			head = create_token(head);
-			cur_token = head;
-		}
-		if (ft_strchr(TOKEN_TYPES, *str))
-			str += set_special_token(str, cur_token, head);
-		else
-			str += set_word_token(str, cur_token, head);
+		cur_token = token_list_append(&head);
+		str += set_token(cur_token, str, head);
 	}
 	return (head);
 }
