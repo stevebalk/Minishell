dd05ce33454102be4fabab0b97bd6c65e5d7b8f0
Jonas Peters <jopeters@c1s4d17.42wolfsburg.de>
2023-11-14 13:52:04 +0100

    load_env() working

--- src/builtins/env_a.c
+++ src/builtins/env_a.c
@@ -6,7 +6,7 @@
 /*   By: jopeters <jopeters@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:22:48 by jopeters          #+#    #+#             */
-/*   Updated: 2023/11/14 13:43:15 by jopeters         ###   ########.fr       */
+/*   Updated: 2023/11/14 13:51:11 by jopeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void show_env_arr(char **env)
 {
+	c_yellow(); printf("show_env_arr()\n"); c_reset();
+
 	int i;
 	i = -1;
 	while(i++, env[i])
@@ -22,13 +24,41 @@ void show_env_arr(char **env)
 
 void load_env(t_list **env_lst, char **env)
 {
+	c_yellow(); printf("load_env()\n"); c_reset();
+
 	(void)env_lst;
 	(void)env;
-/*
 
-
-
-*/
+	int i;
+	i = -1;
+	while(i++, env[i])
+	{
+		//printf("i: %i  >%s<\n", i, env[i]);
+        if (!env_lst)
+            *env_lst = ft_lstnew((void*)env[i]);
+        else
+            ft_lstadd_back(env_lst, ft_lstnew((void*)env[i]));
+	}
 
 	
+}
+
+void show_env_llist(t_list **lst)
+{
+    int i;
+    t_list *tmp_lst;
+    tmp_lst = *lst;
+    
+    i = 0;
+	c_yellow(); printf("show_env_llist()\n"); c_reset();
+    while(tmp_lst)
+    {
+        printf("i: %i  >", i);
+        c_green();
+        printf("%s", (char *)tmp_lst->content);
+        c_reset();
+        printf("<\n");
+        tmp_lst = tmp_lst->next;
+        i++;
+    }
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jopeters <jopeters@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 11:12:12 by jopeters          #+#    #+#             */
-/*   Updated: 2023/11/14 13:43:27 by jopeters         ###   ########.fr       */
+/*   Updated: 2023/11/14 13:48:06 by jopeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,19 @@
 int	main(int argc, char **argv, char **env)
 {
     t_list *hist_llst;
+    t_list *env_llst;
+    
     hist_llst = NULL;
+    env_llst = NULL;
 
     (void)argc;
     (void)argv;
     (void)env;
 
     show_env_arr(env);
-        
+    load_env(&env_llst, env);
+    show_env_llist(&env_llst);
+    
     // history_master(&hist_llst);
     // prompt_handler(&hist_llst);    
     
