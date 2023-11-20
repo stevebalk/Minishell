a2c7e8fae530ea91d7fc1cf47cbace2c2419288e
Jonas Peters <jopeters@c2s17d100.42wolfsburg.de>
2023-11-20 17:28:42 +0100

    main ok

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jopeters <jopeters@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 11:12:12 by jopeters          #+#    #+#             */
-/*   Updated: 2023/11/20 17:23:56 by jopeters         ###   ########.fr       */
+/*   Updated: 2023/11/20 17:28:19 by jopeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,16 @@ int	main(int argc, char **argv, char **env)
 	(void)argv;
 	(void)env;
 
-	char *tmp = get_string_from_first_symbol("add=dfdfuh", '=');
-	printf(">%s<\n", tmp);
-	free(tmp);
-	// signal(SIGINT, handle_sigint);
-	// show_env_arr(env);
-	// load_env_to_llst(&env_llst, env);
-	// copy_llst(&env_llst, &env_llst_sorted);
-	// sort_list(env_llst_sorted);
-	// history_master(&hist_llst);
-	// prompt_handler(&hist_llst, &env_llst, &env_llst_sorted);
-	// lst_dealloc(&hist_llst, 1);
-	// lst_dealloc(&env_llst, 1);
-	// lst_dealloc(&env_llst_sorted, 1);
+	signal(SIGINT, handle_sigint);
+	show_env_arr(env);
+	load_env_to_llst(&env_llst, env);
+	copy_llst(&env_llst, &env_llst_sorted);
+	sort_list(env_llst_sorted);
+	history_master(&hist_llst);
+	prompt_handler(&hist_llst, &env_llst, &env_llst_sorted);
+	lst_dealloc(&hist_llst, 1);
+	lst_dealloc(&env_llst, 1);
+	lst_dealloc(&env_llst_sorted, 1);
 	c_red();
 	printf("~main ()\n");
 	c_reset();
