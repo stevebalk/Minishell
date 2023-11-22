55d12c90f823ad4df4d215a1eded046dbd27cb25
jonaspeters85 <jonaspeters8526@gmail.com>
2023-11-22 17:20:23 +0100

    added a lot of if == NULL conditions and return NULL in get_string ... functions

--- src/utils/util_strings_a.c
+++ src/utils/util_strings_a.c
@@ -6,7 +6,7 @@
 /*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 15:00:37 by jopeters          #+#    #+#             */
-/*   Updated: 2023/11/22 16:58:45 by jonas            ###   ########.fr       */
+/*   Updated: 2023/11/22 17:19:31 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,29 @@
 char	*get_string_till_first_symbol(char *str_in, char symbol)
 {
 	char	*new_str;
+	int		pos_symbol;
 	int		i;
 
+	pos_symbol = get_first_symbol_pos(str_in, symbol);
+	c_yellow(); printf("get_string_til_first_symbol()  str_in >%s<   symbol >%c<  pos symbol: %i\n", str_in, symbol, pos_symbol);
+	if (pos_symbol == -1 && !str_in)
+		return (NULL);
+	if (pos_symbol == -1)
+	{
+		new_str = get_string_from_string(str_in);
+		return (new_str);
+	}
 	i = 0;
 	new_str = str_in;
 
 	while (str_in[i] != symbol)
 		i++;
-	c_yellow(); printf("get_string_til_first_symbol()  str_in >%s<   symbol >%c< \n", str_in, symbol);
 	printf("i: %i \n", i);
 	new_str = (char *)malloc(sizeof(char) * (i + 1));
 	if (!new_str)
 		return (NULL);
 	ft_strlcpy(new_str, str_in, i + 1);
-	c_red();printf("string end >%s<\n", new_str);
+	c_red();printf("~get_string_til_first_symbol() >%s<\n", new_str);
 	return (new_str);
 }
 
@@ -39,19 +48,19 @@ char	*get_string_from_first_symbol(char *str_in, char symbol)
 	char	*new_str;
 	int		pos_symbol;
 	int		len;
-
+	
 	pos_symbol = get_first_symbol_pos(str_in, symbol);
-	if (pos_symbol == -1)
-		return (str_in);
+	c_yellow(); printf("get_string_from_first_symbol()  str_in >%s<   symbol >%c<  pos_symbol: %i\n", str_in, symbol, pos_symbol);
+	if (pos_symbol == -1 || !str_in)
+		return (NULL);
 	len = ft_strlen(str_in);
 	
-	c_yellow(); printf("get_string_from_first_symbol()  str_in >%s<   symbol >%c<  pos_symbol: %i\n", str_in, symbol, pos_symbol);
 	//printf("len %i \n", len);
 	new_str = (char *)malloc(sizeof(char) * (len-pos_symbol + 1));
 	if (!new_str)
 		return (NULL);
 	ft_strlcpy(new_str, str_in + (pos_symbol + 1), (len-pos_symbol + 1));
-	c_red();printf("string end >%s<\n", new_str);
+	c_red();printf("~sget_string_from_first_symbol>%s<\n", new_str);
 	return (new_str);
 }
 
@@ -65,6 +74,8 @@ char	*get_string_without_symbols(char *str_in, char symbol)
 	int		i2;
 	char	*new_str;
 
+	if (!str_in)
+		return (NULL);
 	i = -1;
 	count = 0;
 	while (i++, str_in[i])
@@ -97,6 +108,8 @@ char *get_string_with_symbols_at_end_and_front(char *str_in, char symbol)
 	char	*new_str;
     c_yellow(); printf("get_string_with_symbols_at_end_and_front()  str_in >%s<   symbol >%c< \n", str_in, symbol);
 	fflush(stdout);
+	if (!str_in)
+		return (NULL);
 	i = 0;
 	i2 = 0;
 	new_str = (char *)malloc(sizeof(char) *(ft_strlen(str_in) + 3));
