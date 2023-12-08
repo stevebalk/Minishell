1c357171565ea4b0a28ac18fa99519c853d26a9a
stevebalk <steve.balk@web.de>
2023-12-08 18:39:49 +0100

    Try to fix color promt v2

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 11:16:37 by jopeters          #+#    #+#             */
-/*   Updated: 2023/12/08 18:37:26 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/12/08 18:39:22 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ void	prompt_handler(t_ms *ms)
 	{
 		if (LOGO_ABOVE_PROMPT == 1)
 			intro3();
-		prompt_in = readline("\001\e[0;31m\002minihell\001\e[0;33m\002>>\001\e[0;36m\002");
+		prompt_in = readline("\001\e[0m\002minihell\001\e[0m\002>>\001\e[0m\002");
 		// prompt_in = readline("\001\033[0;31m\002minihell\001\033[0;33m\002>>\001\033[0;36m\002");
 		// prompt_in = readline("\033[0;35mmini(s)hell\033[0;33m >> \033[0;36m");
 
