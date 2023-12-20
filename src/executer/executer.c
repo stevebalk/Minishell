0ba7769fa82ba4b22b4445b8160d9eb9979decc6
stevebalk <steve.balk@web.de>
2023-12-20 15:01:45 +0100

    Redicrection looks ok

--- src/executer/executer.c
+++ src/executer/executer.c
@@ -6,7 +6,7 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:26:39 by sbalk             #+#    #+#             */
-/*   Updated: 2023/12/20 14:32:24 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/12/20 15:00:45 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ void	set_input_io(t_ms *ms, int input_fd, t_cmd_io *cmd_io)
 	{
 		if (dup2(input_fd, STDIN_FILENO) == -1)
 			perror("dup2");
-		close(input_fd);
+		// close(input_fd);
 	}
 }
 
@@ -119,7 +119,7 @@ void	execute_cmd_io(t_ms *ms, t_cmd_io *cmd_io)
 	if (cmd_io == NULL)
 		return ;
 	number_of_commands = 0;
-	input_fd = ms->fd_stdin;
+	input_fd = STDIN_FILENO;
 	cur_cmd_io = cmd_io;
 	while (cur_cmd_io)
 	{
@@ -141,8 +141,7 @@ void	execute_cmd_io(t_ms *ms, t_cmd_io *cmd_io)
 		{
 			if (cur_cmd_io->next)
 			{
-				if (input_fd != STDIN_FILENO)
-					close(input_fd);
+				close(input_fd);
 				input_fd = fds[0];
 				close(fds[1]);
 			}
@@ -156,6 +155,8 @@ void	execute_cmd_io(t_ms *ms, t_cmd_io *cmd_io)
 	i = 0;
 	while (i++ < number_of_commands - 1)
 		waitpid(-1, NULL, 0);
+	dup2(ms->fd_stdin, STDIN_FILENO);
+	dup2(ms->fd_stdout, STDOUT_FILENO);
 	// printf("Exit code: %s\n", ft_itoa(ms->last_exit_code >> 8));
 }
 
