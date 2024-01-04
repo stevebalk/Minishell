23d8fe41fb8f1976c2048c2d124d08bf7d58e12c
jonaspeters85 <jonaspeters8526@gmail.com>
2024-01-04 13:17:16 +0100

    builtin_master new builtin_compare() to save line length

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -6,32 +6,55 @@
 /*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 16:20:53 by jopeters          #+#    #+#             */
-/*   Updated: 2024/01/04 12:21:49 by jonas            ###   ########.fr       */
+/*   Updated: 2024/01/04 13:13:42 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
 #include "../../include/minishell.h"
 
+/* return 1 if the cmd_in comming from the prompt has the same length 
+and characters of cmd_manual */
+static int	builtin_compare(char *cmd_in, char *cmd_manual)
+{
+	int	strlen_cmd_in;
+	int	strlen_cmd_manual;
+
+	strlen_cmd_in = ft_strlen(cmd_in);
+	strlen_cmd_manual = ft_strlen(cmd_manual);
+	if (ft_strncmp(cmd_in, cmd_manual, strlen_cmd_manual) == 0 &&
+		strlen_cmd_in == strlen_cmd_manual)
+		return (1);
+	return (0);
+}
+
 // executes the builtins and return the exit_code of builtin
 int	builtin_master(t_ms *ms, char **cmd_arr)
 {
 	int		exit_code;
-	
+
 	exit_code = 0;
-	if ((ft_strncmp(cmd_arr[0], "cd", 2) == 0) && (ft_strlen(cmd_arr[0]) == 2))
+	//if ((ft_strncmp(cmd_arr[0], "cd", 2) == 0) && (ft_strlen(cmd_arr[0]) == 2))
+	if (builtin_compare(cmd_arr[0], "cd"))
 		exit_code = builtin_cd(ms, NULL, NULL, cmd_arr[1]);
-	if ((ft_strncmp(cmd_arr[0], "echo", 4) == 0) && (ft_strlen(cmd_arr[0]) == 4))
+		
+	//if ((ft_strncmp(cmd_arr[0], "echo", 4) == 0) && (ft_strlen(cmd_arr[0]) == 4))
+	if (builtin_compare(cmd_arr[0], "echo"))
 		exit_code = builtin_echo(cmd_arr);
-	if ((ft_strncmp(cmd_arr[0], "env", 3) == 0) && (ft_strlen(cmd_arr[0]) == 3))
+	//if ((ft_strncmp(cmd_arr[0], "env", 3) == 0) && (ft_strlen(cmd_arr[0]) == 3))
+	if (builtin_compare(cmd_arr[0], "env"))
 		exit_code = show_env_llist(&ms->env_llst);
-	if ((ft_strncmp(cmd_arr[0], "export", 6) == 0) && (ft_strlen(cmd_arr[0]) == 6))
+	//if ((ft_strncmp(cmd_arr[0], "export", 6) == 0) && (ft_strlen(cmd_arr[0]) == 6))
+	if (builtin_compare(cmd_arr[0], "export"))
 		exit_code = export_arg_arr(&ms->env_llst, &ms->env_llst_sorted, cmd_arr);
-	if ((ft_strncmp(cmd_arr[0], "pwd", 3) == 0) && (ft_strlen(cmd_arr[0]) == 3))
+	//if ((ft_strncmp(cmd_arr[0], "pwd", 3) == 0) && (ft_strlen(cmd_arr[0]) == 3))
+	if (builtin_compare(cmd_arr[0], "pwd"))
 		exit_code = builtin_pwd(&ms->env_llst, &ms->env_llst_sorted, 1);
-	if ((ft_strncmp(cmd_arr[0], "unset", 5) == 0) && (ft_strlen(cmd_arr[0]) == 5))
+	//if ((ft_strncmp(cmd_arr[0], "unset", 5) == 0) && (ft_strlen(cmd_arr[0]) == 5))
+	if (builtin_compare(cmd_arr[0], "unset"))
 		exit_code = builtin_unset(ms, cmd_arr);
-	if ((ft_strncmp(cmd_arr[0], "exit", 4) == 0) && (ft_strlen(cmd_arr[0]) == 4))
+	//if ((ft_strncmp(cmd_arr[0], "exit", 4) == 0) && (ft_strlen(cmd_arr[0]) == 4))
+	if (builtin_compare(cmd_arr[0], "exit"))
 		exit_handler(ms);
 	return (exit_code);
 }
