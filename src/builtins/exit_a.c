4ef33beab916a9162dd72e929ea415f145a5afd8
stevebalk <steve.balk@web.de>
2024-01-05 20:39:36 +0100

    Fixed that it should exit always

--- src/builtins/exit_a.c
+++ src/builtins/exit_a.c
@@ -6,7 +6,7 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:43:55 by jopeters          #+#    #+#             */
-/*   Updated: 2024/01/05 18:05:14 by sbalk            ###   ########.fr       */
+/*   Updated: 2024/01/05 20:38:04 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,13 @@
 
 void	exit_handler(t_ms *ms)
 {
-	int	exit_code;
-
-	exit_code = ms->last_exit_code;
 	c_red();
-	write(STDERR_FILENO, "exit\n", 5);
-	c_cyan();
+	ft_putstr_fd("exit\n",STDERR_FILENO);
 	c_reset();
-	free_ms(ms);
-	exit(exit_code);
+	exit_with_code(ms, ms->last_exit_code);
 }
 
-int	builtin_exit(t_ms *ms, char **arr)
+void	builtin_exit(t_ms *ms, char **arr)
 {
 	c_red();
 	fflush(stdout);
@@ -45,10 +40,10 @@ int	builtin_exit(t_ms *ms, char **arr)
 			ft_putstr_fd(arr[1], STDERR_FILENO);
 			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
 			ms->last_exit_code = 255;
-			return (ms->last_exit_code);
+			exit_handler(ms);
 		}
 	}
 	ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
 	ms->last_exit_code = 1;
-	return (ms->last_exit_code);
+	exit_handler(ms);
 }
