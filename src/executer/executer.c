ccaffc3426cae0caa58a80b29a7209c39f0b02c4
stevebalk <steve.balk@web.de>
2024-01-05 10:26:57 +0100

    Removed unused code

--- src/executer/executer.c
+++ src/executer/executer.c
@@ -6,95 +6,44 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:26:39 by sbalk             #+#    #+#             */
-/*   Updated: 2024/01/04 17:30:26 by sbalk            ###   ########.fr       */
+/*   Updated: 2024/01/05 10:25:51 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void print_cmd_io_list(const t_cmd_io *head)
-{
-	const t_cmd_io *current = head;
-
-	while (current != NULL)
-	{
-		printf("Input: %s\n", current->input ? current->input : "NULL");
-		printf("Input Type: %d\n", current->intype);
-		printf("Input File Descriptor: %d\n", current->in_fd);
-		printf("Output: %s\n", current->output ? current->output : "NULL");
-		printf("Output Type: %d\n", current->outtype);
-		printf("Output File Descriptor: %d\n", current->out_fd);
-		printf("Command Array:\n");
-
-		if (current->command_arr != NULL)
-		{
-			for (int i = 0; current->command_arr[i] != NULL; ++i)
-			{
-				printf("  %s\n", current->command_arr[i]);
-			}
-		}
-		else
-		{
-			printf("  NULL\n");
-		}
-
-		printf("Is Valid: %d\n", current->is_valid);
-		printf("Next: %p\n", (void *)current->next);
-
-		current = current->next;
-		ft_putendl_fd("", 1);
-	}
-}
-
-// void	set_input_io(t_ms *ms, int input_fd, t_cmd_io *cmd_io)
+// void print_cmd_io_list(const t_cmd_io *head)
 // {
-// 	int	here_doc_fd[2];
-// 	(void)ms;
-// 	if (cmd_io->intype == TOKEN_INFILE)
-// 	{
-// 		if (dup2(cmd_io->in_fd, STDIN_FILENO) == -1)
-// 			perror("dup2");
-// 	}
-// 	else if (cmd_io->intype == TOKEN_HERE_DOC)
-// 	{
-// 		if (pipe(here_doc_fd) == -1)
-// 			perror("pipe");
-// 		if (dup2(here_doc_fd[1], STDOUT_FILENO) == -1)
-// 			perror("dup2");
-// 		if (dup2(here_doc_fd[0], STDIN_FILENO) == -1)
-// 			perror("dup2");
-// 		write(STDOUT_FILENO, cmd_io->input, ft_strlen(cmd_io->input));
-// 		close(here_doc_fd[0]);
-// 		close(here_doc_fd[1]);
-// 	}
-// 	else
-// 	{
-// 		if (dup2(input_fd, STDIN_FILENO) == -1)
-// 			perror("dup2");
-// 		// close(input_fd);
-// 	}
-// }
+// 	const t_cmd_io *current = head;
 
-// void	set_output_io(t_ms *ms, int fds[2], t_cmd_io *cmd_io)
-// {
-// 	if (cmd_io->outtype == TOKEN_REDIRECT ||
-// 		cmd_io->outtype == TOKEN_REDIRECT_APPEND)
+// 	while (current != NULL)
 // 	{
-// 		if (dup2(cmd_io->out_fd, STDOUT_FILENO) == -1)
-// 			perror("dup2");
-// 		close(cmd_io->out_fd);
+// 		printf("Input: %s\n", current->input ? current->input : "NULL");
+// 		printf("Input Type: %d\n", current->intype);
+// 		printf("Input File Descriptor: %d\n", current->in_fd);
+// 		printf("Output: %s\n", current->output ? current->output : "NULL");
+// 		printf("Output Type: %d\n", current->outtype);
+// 		printf("Output File Descriptor: %d\n", current->out_fd);
+// 		printf("Command Array:\n");
+
+// 		if (current->command_arr != NULL)
+// 		{
+// 			for (int i = 0; current->command_arr[i] != NULL; ++i)
+// 			{
+// 				printf("  %s\n", current->command_arr[i]);
+// 			}
+// 		}
+// 		else
+// 		{
+// 			printf("  NULL\n");
+// 		}
+
+// 		printf("Is Valid: %d\n", current->is_valid);
+// 		printf("Next: %p\n", (void *)current->next);
+
+// 		current = current->next;
+// 		ft_putendl_fd("", 1);
 // 	}
-// 	else if (cmd_io->next)
-// 	{
-// 		if (dup2(fds[1], STDOUT_FILENO) == -1)
-// 			perror("dup2");
-// 	}
-// 	else
-// 	{
-// 		if (dup2(ms->fd_stdout, STDOUT_FILENO) == -1)
-// 			perror("dup2");
-// 	}
-// 	// close(fds[1]); /// <<<< MUST FIX
 // }
 
 void	execute_io(t_ms *ms, t_cmd_io *io)
@@ -126,67 +75,6 @@ void	execute_io(t_ms *ms, t_cmd_io *io)
 	exit_with_code(ms, 127);
 }
 
-// void	execute_cmd_io(t_ms *ms, t_cmd_io *cmd_io)
-// {
-// 	pid_t		pid;
-// 	int			fds[2];
-// 	int			input_fd;
-// 	t_cmd_io	*cur_cmd_io;
-// 	int			i;
-// 	int			number_of_commands;
-
-// 	if (cmd_io == NULL)
-// 		return ;
-// 	number_of_commands = 0;
-// 	input_fd = STDIN_FILENO;
-// 	cur_cmd_io = cmd_io;
-// 	if (!cur_cmd_io->next && cur_cmd_io->command_arr)
-// 	{
-// 		if (is_builtin_command(cur_cmd_io->command_arr[0]))
-// 		{
-// 			builtin_master(ms, cmd_io->command_arr);
-// 			cur_cmd_io = cur_cmd_io->next;
-// 		}
-// 	}
-// 	while (cur_cmd_io)
-// 	{
-// 		if (cur_cmd_io->next)
-// 		{
-// 			if (pipe(fds) == -1)
-// 				perror("pipe");
-// 		}
-// 		pid = fork();
-// 		if (pid == -1)
-// 			perror("fork");
-// 		if (pid == 0)
-// 		{
-// 			set_input_io(ms, input_fd, cur_cmd_io);
-// 			set_output_io(ms, fds, cur_cmd_io);
-// 			execute_io(ms, cur_cmd_io);
-// 		}
-// 		else
-// 		{
-// 			if (cur_cmd_io->next)
-// 			{
-// 				close(input_fd);
-// 				input_fd = fds[0];
-// 				close(fds[1]);
-// 			}
-// 			else if (cur_cmd_io->next == NULL && input_fd != STDIN_FILENO)
-// 				close(input_fd);
-// 		}
-// 		cur_cmd_io = cur_cmd_io->next;
-// 		number_of_commands++;
-// 	}
-// 	waitpid(pid, &ms->last_exit_code, 0);
-// 	i = 0;
-// 	while (i++ < number_of_commands - 1)
-// 		waitpid(-1, NULL, 0);
-// 	dup2(ms->fd_stdin, STDIN_FILENO);
-// 	// dup2(ms->fd_stdout, STDOUT_FILENO);
-// 	// printf("Exit code: %s\n", ft_itoa(ms->last_exit_code >> 8));
-// }
-
 void init_cmd_io(t_cmd_io *io)
 {
 	io->command_arr = NULL;
@@ -200,37 +88,10 @@ void init_cmd_io(t_cmd_io *io)
 	io->is_valid = 1;
 }
 
-// static char	*get_heredoc_string(t_ms *ms, t_redir *redir)
-// {
-// 	t_redir *cur;
-// 	char	*ret;
-
-// 	if (redir == NULL)
-// 		return (NULL);
-// 	ret = NULL;
-// 	cur = redir;
-// 	while (cur)
-// 	{
-// 		if (cur->type == TOKEN_HERE_DOC)
-// 		{
-// 			if (ret)
-// 				free(ret);
-// 			ret = heredoc(cur->target, ms);
-// 		}
-// 		cur = cur->next;
-// 	}
-// 	return (ret);
-// }
-
 static int redir_infile(t_redir *redir, t_cmd_io *io)
 {
 	if (io->in_fd != -1)
 		close(io->in_fd);
-	// if (io->intype && io->intype == TOKEN_HERE_DOC)
-	// {
-	// 	free(io->input);
-	// 	io->input = NULL;
-	// }
 	io->in_fd = open(redir->target, O_RDONLY, 0644);
 	if (io->in_fd == -1)
 	{
@@ -353,11 +214,6 @@ void set_input_io(int input_fd, int fds[2], t_cmd_io *cmd_io)
 		if (dup2(input_fd, STDIN_FILENO) == -1)
 			perror("Dup2: Error set_input_io: input_fd to STDIN");
 	}
-	// if (input_fd != STDIN_FILENO)
-	// {
-	// 	close(input_fd);
-	// 	close(fds[0]);
-	// }
 }
 
 void set_output_io(t_ms *ms, int fds[2], t_cmd_io *cmd_io)
@@ -372,10 +228,7 @@ void set_output_io(t_ms *ms, int fds[2], t_cmd_io *cmd_io)
 	else if (ms->cmd->next)
 	{
 		if (dup2(fds[1], STDOUT_FILENO) == -1)
-		{
 			perror("Dup2: Error set_output_io: TOKEN_PIPE");
-			// close(fds[1]);
-		}
 	}
 	else
 	{
