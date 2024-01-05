7c47352409696a3f5c6196521fb7db796788032d
stevebalk <steve.balk@web.de>
2024-01-05 19:28:41 +0100

    Fixed echo not checking for multiple -n

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,39 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
+/*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 16:43:21 by jonas             #+#    #+#             */
-/*   Updated: 2024/01/04 15:29:42 by jonas            ###   ########.fr       */
+/*   Updated: 2024/01/05 19:27:46 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
+#include "minishell.h"
 
 int	builtin_echo(char **str_arr)
 {
-	int	exit_code;
-	int	i;
-	int	n_flag;
+	size_t	i;
+	size_t	n_flag;
 
-	exit_code = 0;
-	i = 0;
+	i = 1;
 	n_flag = 0;
-	if (!str_arr)
-		return (0);
-	if (str_arr[1] && ft_strncmp(str_arr[1], "-n", 2) == 0
-		&& ft_strlen(str_arr[1]) == 2)
+	while (str_arr && str_arr[i])
 	{
-		n_flag = 1;
+		if (ft_strncmp(str_arr[i], "-n", 2) == 0
+			&& ft_strlen(str_arr[i]) == 2)
+			n_flag = 1;
+		else
+			break ;
 		i++;
 	}
-	while (i++, str_arr[i])
+	while (str_arr && str_arr[i])
 	{
-		if (i > 1)
-			printf(" ");
-		printf("%s", str_arr[i]);
+		ft_putstr_fd(str_arr[i], STDOUT_FILENO);
+		if (i < get_size_of_array(str_arr) - 1)
+			ft_putstr_fd(" ", STDOUT_FILENO);
+		i++;
 	}
 	if (!n_flag)
-		printf("\n");
-	return (exit_code);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+	return (0);
 }
