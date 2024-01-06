15c61e0b82d7a52500b1a0d4f31bb7e51029a69f
stevebalk <steve.balk@web.de>
2024-01-06 17:11:06 +0100

    Removed valgrind scripts

--- valgrind_check.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-valgrind --suppressions=valgrind_readline_leaks_ignore.txt --leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes --verbose --log-file=valgrind-out.txt ./minishell
